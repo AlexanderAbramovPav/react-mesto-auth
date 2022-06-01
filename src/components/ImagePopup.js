851f8a869e2493bf0b00591832ce35d74f94ce90
AlexanderAbramovPraktikum <95632162+AlexanderAbramovPraktikum@users.noreply.github.com>
2022-06-01 13:29:19 +0300

    full_pr10_fix1.1

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -7,11 +7,11 @@ function ImagePopup(props) {
         <div className={props.card ? `popup popup-img popup_opened` : `popup popup-img`} tabIndex={-1}>
           <div className="popup__img-container">
             <img
-                src={props.card.link} // не понимаю, как сделать так, чтобы движок при монтировании игнорил null значение
-                alt={props.card.name} // не понимаю, как сделать так, чтобы движок при монтировании игнорил null значение
+                src={props.card === null ? "#" : props.card.link} // не понимаю, как сделать так, чтобы движок при монтировании игнорил null значение
+                alt={props.card === null ? "" : props.card.name} // не понимаю, как сделать так, чтобы движок при монтировании игнорил null значение
                 className="popup__image"
               />
-              <p className="popup__img-title">{props.card.name}</p>
+              <p className="popup__img-title">{props.card === null ? "" : props.card.name}</p>
               <button className="popup__close-btn popup-img-close-btn" type="button" aria-label="Закрыть карточку" onClick={props.onClose}></button>
           </div>
         </div>
